91ec3c6dd0bae473792717397ab1c2117df66afe
amine zouhaid <amine2020zouhaid@gmail.com>
2022-06-23 12:18:54 +0100

    home page modified

--- front-end/src/compenent/Home.js
+++ front-end/src/compenent/Home.js
@@ -18,6 +18,40 @@ export default function Home() {
   
     <a href=""><img className='flech' src={require("./img/flech.png")} alt="" /></a>
    </div>
+
+   <div className='feedback'>
+
+   <div className="col">
+  <div className='imagesB'><img  src="https://thebarbercompany.fr/wp-content/uploads/2019/07/Icon_club.png" alt /></div>
+  <p className="titleC">The Barber Club</p>
+  <a href="#"  className="cta-btn-gold">Rejoindre le club</a>
+</div>
+
+<div className="col">
+ <div className='imagesB'> <img src="https://thebarbercompany.fr/wp-content/uploads/2019/07/Icon_salon.png" alt /> </div>
+  <p className="titleC">Les Salons</p>
+  <a href="#"  className="cta-btn-gold">Où sommes-nous ?</a>
+</div>
+
+
+<div className="col">
+ <div className='imagesB'> <img src="https://thebarbercompany.fr/wp-content/themes/thebarbercompany/assets/img/Icon_newsletter.png" alt /> </div>
+  <p className="titleC">Newsletter</p>
+  <a  href="#" className="cta-btn-gold btn-newslatter">S'abonner</a>
+</div>
+
+<div className="col">
+<div className='imagesB'> <img src="https://thebarbercompany.fr/wp-content/themes/thebarbercompany/assets/img/Icon_reseaux_sociaux.png" alt /></div> 
+  <p className="titleC">Suivez-Nous</p>
+  <div className="resauxS">
+    <a href="#" target="_blank"><img src="https://thebarbercompany.fr/wp-content/uploads/2019/08/Icon_instagram_orange.png" alt /></a>
+    <a href="#" target="_blank"><img src="https://thebarbercompany.fr/wp-content/uploads/2019/08/Icon_facebook_orange.png" alt /></a>
+    <a href="#" target="_blank"><img src="https://thebarbercompany.fr/wp-content/uploads/2019/08/Icon_pinterest_orange.png" alt /></a>
+  </div>
+</div>
+
+
+   </div>
    
    <div className='images'>
     <img className='img1' src={require("./img/a.jpg")} alt="" />
@@ -145,11 +179,82 @@ export default function Home() {
     </div>
   </div>
 </div>
+</div>
 
 
+<div className='prestation'>
+  <h1 className='grandTitle'> Nos Produits </h1>
+  <div className='allproducte'>
 
+  <div className="col-3 style1 Tous  coiffants ">
+  <div className="hovereffect">
+    <div className="content-txt">
+      <p className="title">Huile à barbe</p>
+      <h2 className="disc">BEARD OIL</h2>
+    </div>
+    <img className="img-responsive" src="https://thebarbercompany.fr/wp-content/uploads/2019/09/2019-10_571x571_BEARDOIL_TBC.jpg" alt />
+    
+  </div>
+</div>
+
+
+<div className="col-3 style1 Tous  coiffants ">
+  <div className="hovereffect">
+    <div className="content-txt">
+      <p className="title">Poudre Texturisante</p>
+      <h2 className="disc">DESIGN POWDER</h2>
+    </div>
+    <img className="img-responsive" src="https://thebarbercompany.fr/wp-content/uploads/2021/12/DESIGN-POWDER.jpg" alt />
+    
+  </div>
+</div>
+
+<div className="col-3 style1 Tous  coiffants ">
+  <div className="hovereffect">
+    <div className="content-txt">
+      <p className="title">Pâte Modelante</p>
+      <h2 className="disc">DESIGN PASTE</h2>
+    </div>
+    <img className="img-responsive" src="https://thebarbercompany.fr/wp-content/uploads/2021/03/BARBER-COMPANY-PASTE-V2.jpg" alt />
+    
+  </div>
+</div>
+
+
+  </div>
 
 </div>
+
+<div className='allCostmers'>
+  <div className="containerB">
+    <div className="mgb-40 padb-30 auto-invert line-b-4 align-center">
+      <h4 className="font-cond-l fg-accent lts-md mgb-10" contentEditable="false">Not Yet Convinced?</h4>
+      <h1 className="font-cond-b fg-text-d lts-md fs-300 fs-300-xs no-mg" contentEditable="false">Read Customer Reviews</h1>
+    </div>
+    <ul className="hash-list cols-3 cols-1-xs pad-30-all align-center text-sm">
+      <li>
+        <img src="https://bootdey.com/img/Content/avatar/avatar1.png" className="wpx-100 img-round mgb-20" title alt data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]" />
+        <p className="fs-110 font-cond-l" contentEditable="false">" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. "</p>
+        <h5 className="font-cond mgb-5 fg-text-d fs-130" contentEditable="false">Martha Stewart</h5>
+        <small className="font-cond case-u lts-sm fs-80 fg-text-l" contentEditable="false">Business Woman - New York</small>
+      </li>
+      <li>
+        <img src="https://bootdey.com/img/Content/avatar/avatar4.png" className="wpx-100 img-round mgb-20" title alt data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]" />
+        <p className="fs-110 font-cond-l" contentEditable="false">" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. "</p>
+        <h5 className="font-cond mgb-5 fg-text-d fs-130" contentEditable="false">Ariana Menage</h5>
+        <small className="font-cond case-u lts-sm fs-80 fg-text-l" contentEditable="false">Recording Artist - Los Angeles</small>
+      </li>
+      <li>
+        <img src="https://bootdey.com/img/Content/avatar/avatar5.png" className="wpx-100 img-round mgb-20" title alt data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]" />
+        <p className="fs-110 font-cond-l" contentEditable="false">" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. "</p>
+        <h5 className="font-cond mgb-5 fg-text-d fs-130" contentEditable="false">Sean Carter</h5>
+        <small className="font-cond case-u lts-sm fs-80 fg-text-l" contentEditable="false">Fund Manager - Chicago</small>
+      </li>
+    </ul>
+  </div>
+</div>
+
+
    <Footer/>
    
     </div>
